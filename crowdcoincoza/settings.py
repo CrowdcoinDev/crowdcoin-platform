1c5e120a33878cd4d49c3f9145066d5bf4074c28
Kearabiloe Ledwaba <kearabiloe.ledwaba@gmail.com>
2024-07-15 18:03:59 +0200

    Debug

--- crowdcoincoza/settings.py
+++ crowdcoincoza/settings.py
@@ -17,6 +17,7 @@ import os
 from logentries import LogentriesHandler
 import dj_database_url
 import environ
+from django.core.management.utils import get_random_secret_key
 
 env = environ.Env(
     # set casting, default value
@@ -32,7 +33,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',get_random_secret_key())
 print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
